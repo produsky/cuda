541cdb50e3e5d0d03167d8e9a7ab6e91007c2721
produsky <maxime.2412@live.fr>
2022-04-23 17:07:56 +0200

    e

--- grayscale.cu
+++ grayscale.cu
@@ -10,11 +10,11 @@ __global__ void grayscale_pixels(
     int y = blockIdx.y * blockDim.y + threadIdx.y;
 
     if(x < cols && y < rows) {
-        int red = rgb[3 * (y * cols + x)];
-        int green = rgb[3 * (y * cols + x) + 1];
-        int blue = rgb[3 * (y * cols + x) + 2];
+        int red = pixels_rgb_directive[3 * (y * cols + x)];
+        int green = pixels_rgb_directive[3 * (y * cols + x) + 1];
+        int blue = pixels_rgb_directive[3 * (y * cols + x) + 2];
 
-        grayscale_pixels_directive[y * cols + x] = (307 * red + 604 * green + 113 * blue) / 1024;
+        gray_pixels_directive[y * cols + x] = (307 * red + 604 * green + 113 * blue) / 1024;
     }
 }
 
@@ -22,7 +22,7 @@ __global__ void grayscale_pixels(
 int main() {
     cv::Mat input_image = cv::imread("in.jpg", cv::IMREAD_UNCHANGED);
     unsigned char* pixels_rgb = input_image.data;
-    std:vector<unsigned char> gray_pixels(input_image.rows * input_image.cols);
+    std::vector<unsigned char> gray_pixels(input_image.rows * input_image.cols);
     cv::Mat output_image(input_image.rows, input_image.cols, CV_8UC1, gray_pixels.data());
     
     unsigned char* pixels_rgb_directive;
@@ -37,7 +37,7 @@ int main() {
 
     grayscale_pixels<<<nb_blocks, nb_threads>>>(pixels_rgb_directive, gray_pixels_directive, input_image.cols, input_image.rows);
 
-    cudaMemcpy(gray_pixels.data(), gray_pixels_directive, rows * cols, cudaMemcpyDeviceToHost);
+    cudaMemcpy(gray_pixels.data(), gray_pixels_directive, input_image.rows * input_image.cols, cudaMemcpyDeviceToHost);
 
     cv::imwrite("out.jpg", output_image);
 
